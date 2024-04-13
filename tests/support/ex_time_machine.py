e0f7535d817dc549d0cd85a5a9f2d410aed35d4d
Bohdan Danishevsky <dbn@aminis.com.ua>
2024-04-13 01:14:12 +0100

    Simplification for time mocking

---
+++ tests/support/ex_time_machine.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+from unittest import mock
+
+import time_machine
+
+
+@contextmanager
+def travel(destination, *args, **kwargs):
+    assert isinstance(destination, int) or isinstance(destination, float)
+
+    tick = kwargs.get("tick", True)
+    tick_delta = kwargs.get("tick_delta", 0.500)
+
+    if "tick_delta" in kwargs:
+        kwargs.pop("tick_delta")
+
+    current_monotonic_time_value = destination
+
+    def mocked_monotonic_implementation() -> float:
+        nonlocal current_monotonic_time_value
+        result = current_monotonic_time_value
+
+        if tick:
+            current_monotonic_time_value += tick_delta
+
+        return result
+
+    with time_machine.travel(destination, *args, **kwargs), \
+            mock.patch("time.monotonic", side_effect=mocked_monotonic_implementation), \
+            mock.patch(
+                "time.monotonic_ns",
+                side_effect=lambda: int(10_000_000_000 * mocked_monotonic_implementation())
+            ):
+
+        yield
